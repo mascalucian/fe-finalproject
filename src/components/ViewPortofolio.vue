02408ef567dfa891c3476d8702dea6f30d6b95b7
memesCuMaicata <88316605+alXDraghiciu@users.noreply.github.com>
2021-08-26 14:37:48 +0300

    change cover picture css to match viewComp

--- src/components/ViewPortofolio.vue
+++ src/components/ViewPortofolio.vue
@@ -298,7 +298,7 @@ html {
 
 .wrapper {
   position: absolute;
-  // background-image: url(https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1907&q=80);
+  background-image: url(https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1907&q=80);
   background-repeat: no-repeat;
   background-position: top;
   background-size: cover;
