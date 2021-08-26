7bf7de0b97ed53f0934ae5d2b2141426c9811c32
memesCuMaicata <88316605+alXDraghiciu@users.noreply.github.com>
2021-08-26 10:33:43 +0300

    added vue-google-maps for ContactComp

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,34 @@
-<template><p>Contact works!</p></template>
+<template
+  ><div>
+    <header>
+      <h1>Contact us!</h1>
+    </header>
+    <main>
+      <div>
+        <h3>
+          For any business related inquiries, please feel free to contact us at:
+        </h3>
+        <a>bussiness@portofol.io</a>
+      </div>
+      <div>
+        <h3>
+          Found a bug? Problems with your portofol.io? Contact us at:
+        </h3>
+        <a>support@portofol.io</a>
+      </div>
+      <div></div>
+    </main></div
+></template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      map: undefined,
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import {
-    createApp
-} from "vue";
+import { createApp } from "vue";
 import "firebase/firestore";
 import store from "./store";
 import router from "./config/router";
 import VueLoading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
+import VueGoogleMap from "vuejs-google-maps";
+import "vuejs-google-maps/dist/vuejs-google-maps.css";
 
 import App from "./App.vue";
 
@@ -13,11 +13,21 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 app.use(
-    VueLoading, {
-        color: "#fd7905",
-        backgroundColor: "#fed28a",
-    }, {
-        // slots
-    }
+  VueLoading,
+  {
+    color: "#fd7905",
+    backgroundColor: "#fed28a",
+  },
+  {
+    // slots
+  }
 );
+app.use(VueGoogleMap, {
+  load: {
+    apiKey: "AIzaSyChAjp5DnMOrs3YFbiR2-RYtjKSiOWkG5E",
+    libraries: [
+      /* rest of libraries */
+    ],
+  },
+});
 app.mount("#app");
